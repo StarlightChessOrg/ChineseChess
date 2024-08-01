05d4b90a94bec2b21ad44ec60ffac2cceca005f8
Ways49 <1436980420@qq.com>
2024-08-01 13:15:10 +0800

    fix a bug

--- bit_src/bitboard.hpp
+++ bit_src/bitboard.hpp
@@ -23,8 +23,9 @@ public:
         readFromBoard(board);
     }
     void initPublicResource(){
-        for(int i = 3;i < 12;i++){
-            for(int a = 3;a < 12;a++){
+        //初始化判断棋子间障碍的屏蔽位
+        for(int i = 3;i < 13;i++){
+            for(int a = 3;a < 13;a++){
                 for(int from = min(i,a) + 1;from < max(i,a);from++){
                     mayExistBarrier[i][a] |= ((uint16)1 << from);
                 }
@@ -33,6 +34,7 @@ public:
     }
     void readFromBoard(const int board[256]){
         clearBitBoard();
+        //通过外部数组，初始化位棋盘
         for(int pos = 51;pos < 205;pos++){
             if(board[pos]){
                 yBits[getY(pos)] |= ((uint16)1 << (getX(pos)));
@@ -41,14 +43,17 @@ public:
         }
     }
     void clearBitBoard(){
+        //清空数组
         memset(yBits,(uint16)0,16 * sizeof(uint16));
         memset(xBits,(uint16)0,16 * sizeof(uint16));
     }
     void printBitBoard(bool printTranspose = true){
+        //打印整个棋盘的bit位
         cout<<"yBits:"<<endl;
         for(unsigned short yBit : yBits){
             printBitLine(yBit);
         }
+        //打印转置棋盘的bit位
         if(printTranspose){
             cout<<endl;
             cout<<"xBits:"<<endl;
@@ -58,6 +63,7 @@ public:
         }
     }
     static void printBitLine(uint16 Bits){
+        //打印单行的bit位
         for(int i = 0;i < 16;i++){
             if(Bits & ((uint16)1 << i)){
                 cout <<1;
@@ -73,18 +79,21 @@ public:
 public:
     bool checkLineExistBarrier(int yFrom,int xFrom,int yTo,int xTo){
         assert((xFrom != xTo) || (yFrom != yTo));
+        //检查啷个棋子中间是否有障碍物
         if(xFrom != xTo){
             return mayExistBarrier[xFrom][xTo] & yBits[yFrom];
         }
         return mayExistBarrier[yFrom][yTo] & xBits[xFrom];
     }
     void makeMove(int yFrom,int xFrom,int yTo,int xTo){
+        //步进
         yBits[yFrom] &= ~((uint16)1 << xFrom);
         yBits[yTo] |= ((uint16)1 << xTo);
         xBits[xFrom] &= ~((uint16)1 << yFrom);
         xBits[xTo] |= ((uint16)1 << yTo);
     }
     void unMakeMove(int yFrom,int xFrom,int yTo,int xTo){
+        //撤销步进
         yBits[yFrom] |= ((uint16)1 << xFrom);
         yBits[yTo] &= ~((uint16)1 << xTo);
         xBits[xFrom] |= ((uint16)1 << yFrom);
@@ -92,9 +101,11 @@ public:
     }
 protected:
     static int getX(int pos){
+        //获得棋子的列索引
         return pos & 15;
     }
     static int getY(int pos){
+        //获得棋子的行索引
         return (pos >> 4);
     }
 protected:
